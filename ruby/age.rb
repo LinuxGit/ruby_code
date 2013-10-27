4d9338a7fd24051a0a5b30ba24fffb3926a273fe
LinuxGit <gofaceme@gmail.com>
2013-10-27 23:24:47 +0800

    add birth_year variable

--- ruby/age.rb
+++ ruby/age.rb
@@ -1,11 +1,22 @@
-birthdate = 1990
+this_year  = 2013
+birth_year = gets.to_i
 
-generation = case  birthdate
-when  1980..1989 then "80's"
-when  1990..1999 then "90's"
-when  2000..2013 then "00's"
-else  "The old."
+if birth_year > 2013
+  puts "Are you kidding?"
+  exit
+elsif birth_year <= 0
+  puts "The year of your birth can't small than 0 or equal 0"
+  exit
+else
+  puts "You born in #{birth_year}"
 end
 
-age = 2013 - birthdate + 1
-puts "Yo are #{generation} and #{age} years old."
+generation = case  birth_year
+             when  1980..1989 then "80's"
+             when  1990..1999 then "90's"
+             when  2000..2013 then "00's"
+             else  "the old."
+             end
+
+age = this_year - birth_year + 1
+puts "You are #{generation} and #{age} years old."
