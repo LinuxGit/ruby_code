8d1457dcae4d4716aa4fa96dbd089c1d8e6e994b
LinuxGit <gofaceme@gmail.com>
2014-10-25 17:42:42 +0800

    ruby method

---
+++ ruby/RubyProgramming/7_method/keyword_arguments.rb
@@ -0,0 +1,6 @@
+def area(x: 0, y: 0, z: 0)
+  (x*y + x*z + y*z) * 2
+end
+
+args1= {x: 1, y: 2, z: 3}
+p area(args1)
---
+++ ruby/RubyProgramming/7_method/myloop.rb
@@ -0,0 +1,12 @@
+def myloop
+  while true
+    yield
+  end
+end
+
+num = 1
+myloop do
+  break if num > 100
+  puts "num is #{num}."
+  num *= 2
+end
