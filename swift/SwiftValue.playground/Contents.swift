056c48e9662b303654dd51aa8b7d115515f9abcb
Louis <gofaceme@gmail.com>
2015-07-12 01:14:48 +0800

    swift tour

---
+++ swift/SwiftValue.playground/Contents.swift
@@ -0,0 +1,96 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Simple Values
+var str = "Hello, playground"
+
+print("Hello, Swift.")
+
+let maxNum = 1000
+
+var index = 2
+
+var websiteName:String
+websiteName = "www.imooc.com"
+
+
+var a:Int = 3
+var b:Double = 0.1415926
+var pi:Double = Double(a) + b
+
+let implicitFloat:Float = 4
+
+let label = "The width is "
+let width = 96
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 6
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) fruit."
+
+let cost = "I spent \(8 + 0.7) RMB on lunch."
+var name = "Louis"
+var greet = "Welcome to LinuxGit, \(name)."
+
+var linuxList = ["CentOS", "RedHat"]
+linuxList[1] = "Ubuntu"
+print(linuxList)
+
+var occupations = [
+    "Louis": "Enginner",
+    "Maggie": "Saleswowan"
+]
+occupations["Angelo"] = "Teacher"
+print(occupations)
+
+var emptyArray = [String]()
+emptyArray[0] = "Louis"
+
+let emptyDictionary = [String:Float]()
+
+var shopList = []
+var jobs = [:]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
