05ce9bb15219f56259736293696b03deaaed62fe
Louis <gofaceme@gmail.com>
2015-08-02 19:04:20 +0800

    swift operator

---
+++ swift/FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by ruby on 15/8/2.
+//  Copyright © 2015年 ruby. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+}
+
--- swift/Operator.playground/Contents.swift
+++ swift/Operator.playground/Contents.swift
@@ -1,7 +1,5 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
 let b = 5
 var a = 10
 a = b
@@ -19,3 +17,37 @@ var c = 0
 let d = ++c
 let e = c++
 let f = --c
+
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
+// Nil Coalesing Operator
+let defaultColorName = "red"
+var userDefinedColorName: String?
+
+userDefinedColorName = "green"
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+// Range
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5).")
+}
+
+let names = ["Apple", "Google", "Yahoo", "Quora"]
+let count = names.count
+for i in 0..<count {
+    print("Company \(i + 1) is called \(names[i])")
+}
+
+// Logical Operators
+let enteredDoorCode = true
+let passedRetinaScan = false
+let hasDoorKey = false
+let knowsOverridePassword = true
+
+if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED!")
+}
