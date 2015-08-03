c437070f1add399458a0006225c14cfa433112c7
Louis <gofaceme@gmail.com>
2015-08-04 00:41:38 +0800

    swift String and Characters

---
+++ swift/StringsAndCharacters.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("Nothing to see here.")
+}
+
+let catCharacters: [Character] = ["C", "a", "t", "!"]
+let catString = String(catCharacters)
+
+var welcome = "hello"
+let exclamationMark: Character = "!"
+welcome.append(exclamationMark)
+
