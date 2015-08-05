b85a46ec3da9eda37a5dba564150e3d3e55da33c
Louis <gofaceme@gmail.com>
2015-08-05 21:52:27 +0800

    swift string

--- swift/StringsAndCharacters.playground/Contents.swift
+++ swift/StringsAndCharacters.playground/Contents.swift
@@ -13,3 +13,18 @@ var welcome = "hello"
 let exclamationMark: Character = "!"
 welcome.append(exclamationMark)
 
+let wiseWords = "\"Imagination is more important than knowleage\"-Einstein"
+let dollarSign = "\u{24}"
+let sparkingHeart = "\u{1F496}"
+
+var word = "cafe"
+word.characters.count
+
+let greeting = "ruby china"
+print(greeting.startIndex)
+print(greeting.endIndex)
+
+
+
+
+
