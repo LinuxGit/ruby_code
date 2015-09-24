0cbc9e22a297cd51583e9528ceb1d019c40c9d6f
Louis <gofaceme@gmail.com>
2015-09-24 23:25:58 +0800

    Swift Operator

--- swift/SwiftOperator.playground/Contents.swift
+++ swift/SwiftOperator.playground/Contents.swift
@@ -1,5 +1,4 @@
-//: Playground - noun: a place where people can play
-
+//Asignment Operator
 let b = 5
 var a = 10
 a = b
@@ -7,17 +6,21 @@ a = b
 let (x, y) = (1, 2)
 x
 
+//Arithmetic Operator
 "hello " + "world"
 
+//Reminder Operator
 9 % 4
 -9 % 4
 8 % 2.5
 
+//Increment and Decrement Operators
 var c = 0
 let d = ++c
 let e = c++
 let f = --c
 
+//Ternary Conditional Operator
 let contentHeight = 40
 let hasHeader = true
 let rowHeight = contentHeight + (hasHeader ? 50 : 20)
@@ -26,10 +29,10 @@ let rowHeight = contentHeight + (hasHeader ? 50 : 20)
 let defaultColorName = "red"
 var userDefinedColorName: String?
 
-userDefinedColorName = "green"
+//userDefinedColorName = "green"
 var colorNameToUse = userDefinedColorName ?? defaultColorName
 
-// Range
+//Range
 for index in 1...5 {
     print("\(index) times 5 is \(index * 5).")
 }
