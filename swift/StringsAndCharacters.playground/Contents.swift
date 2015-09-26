0270fadba142205420895103be6d6af70fdfc291
Louis <gofaceme@gmail.com>
2015-09-27 06:22:57 +0800

    Swift String and Characters

--- swift/StringsAndCharacters.playground/Contents.swift
+++ swift/StringsAndCharacters.playground/Contents.swift
@@ -1,4 +1,4 @@
-//: Playground - noun: a place where people can play
+//Emtpy String
 var emptyString = ""
 var anotherEmptyString = String()
 
@@ -6,35 +6,51 @@ if emptyString.isEmpty {
     print("Nothing to see here.")
 }
 
+//Characters
 let catCharacters: [Character] = ["C", "a", "t", "!"]
 let catString = String(catCharacters)
 
+for character in catString.characters {
+    print(character)
+}
+
+//Concatenating Strings and Characters
 var welcome = "hello"
 let exclamationMark: Character = "!"
 welcome.append(exclamationMark)
 
+//Unicode Scalars
 let wiseWords = "\"Imagination is more important than knowleage\"-Einstein"
 let dollarSign = "\u{24}"
 let sparkingHeart = "\u{1F496}"
 
+//Characters count
 var word = "cafe"
 word.characters.count
 
+//String indices
 var greeting = "ruby china"
 print(greeting.startIndex)
 print(greeting.endIndex)
 greeting[greeting.startIndex.successor()]
 greeting[greeting.endIndex.predecessor()]
-let index = advance(greeting.startIndex, 5)
+let index = greeting.startIndex.advancedBy(6)
 greeting[index]
 
+//String inserting and removing
 greeting.insert("!", atIndex: greeting.endIndex)
-greeting.removeAtIndex(welcome.endIndex.predecessor())
-print(greeting)
+greeting.insertContentsOf(" party".characters, at: greeting.endIndex.predecessor())
 
+greeting.removeAtIndex(greeting.endIndex.predecessor())
+let range = greeting.endIndex.advancedBy(-6)..<greeting.endIndex
+greeting.removeRange(range)
+
+"ruby" == "ruby"
+"ruby" != "ruby"
+
+//Prefix and Suffix
 let languages = ["PHP", "Python", "Ruby", "Perl"]
-var pCount = 0
-var yCount = 0
+var pCount = 0, yCount = 0
 for language in languages {
     if language.hasPrefix("P") {
         ++pCount
@@ -43,8 +59,6 @@ for language in languages {
         ++yCount
     }
 }
-
-
 
 
 
