c38901d02c909caa4c0563a7edac843f62cbe25d
Louis <gofaceme@gmail.com>
2015-10-14 00:07:01 +0800

    FoodTracker Build a Basic UI

---
+++ swift/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by ruby on 15/10/12.
+//  Copyright © 2015年 LinuxGit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
