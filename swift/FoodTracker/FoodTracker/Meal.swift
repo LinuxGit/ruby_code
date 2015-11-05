d90ff80df13e1db2a78f5018585809b3d5d12080
Louis <gofaceme@gmail.com>
2015-11-05 23:48:25 +0800

    Swift Functions

--- swift/FoodTracker/FoodTracker/Meal.swift
+++ swift/FoodTracker/FoodTracker/Meal.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
     
     var name: String
@@ -20,8 +20,42 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
     }
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentationDirectory, inDomains: .UserDomainMask).first!
+    
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer
+        self.init(name: name, photo: photo, rating: rating)
+    }    
+    
 }
--- swift/SwiftFunctions.playground/Contents.swift
+++ swift/SwiftFunctions.playground/Contents.swift
@@ -1,16 +1,16 @@
-//Defining Functions
+// Defining Functions
 func sayHello(personName: String) -> String {
     return "Hello, " + personName + "!"
 }
 print(sayHello("Anna"))
 
-//Functions Without Parameters
+// Functions Without Parameters
 func sayHelloWorld() -> String {
     return "Hello World."
 }
 print(sayHelloWorld())
 
-//Functions with multiple parameters
+// Functions with multiple parameters
 func sayHello(personName: String, alreadyGreeted: Bool) -> String {
     if alreadyGreeted {
         return "Hello again, \(personName)."
@@ -20,9 +20,135 @@ func sayHello(personName: String, alreadyGreeted: Bool) -> String {
 }
 print(sayHello("Louis", alreadyGreeted: true))
 
-//Functions Without Return Values
+// Functions Without Return Values
 func sayGoodbye(personName: String) {
     print("Goodbye, \(personName).")
 }
-
 sayGoodbye("Louis")
+
+// Functions with Multiple Return Values and Optional Tuple Return Types
+func minMax(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return nil }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    
+    return (min: currentMin, max: currentMax)
+}
+
+if let bounds = minMax([9, -1, 3, 7, 12]) {
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+
+// Specifying External Parameter Names
+func sayHello(to person: String, and anotherPerson: String) -> String {
+    return "Hello \(person) and \(anotherPerson)!"
+}
+print(sayHello(to: "Louis", and: "Ruby"))
+
+// Variadic Parameters
+func arithmethicMean(numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    
+    return total / Double(numbers.count)
+}
+print(arithmethicMean(1, 2, 3, 4, 5))
+
+// Constant and Variable Parameters
+func alignRight(var string: String, totalLength: Int, pad: Character) -> String {
+    let amountToPad = totalLength - string.characters.count
+    if amountToPad < 1 {
+        return string
+    }
+    let padString = String(pad)
+    for _ in 1...amountToPad {
+        string = padString + string
+    }
+    
+    return string
+}
+
+let originalString = "hello"
+let paddedString = alignRight(originalString, totalLength: 10, pad: "-")
+
+// In-Out Parameters
+func swapTwoInts(inout a: Int, inout _ b: Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 77
+var anotherInt = 9
+swap(&someInt, &anotherInt)
+print("Now someInt is \(someInt), anotherInt is \(anotherInt).")
+
+
+// Function Types
+func addTwoInts(a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func multiplyTwoInts(a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+var mathFunction: (Int, Int) -> Int = addTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+// Function Types as Parameter Types
+func printMathResult(mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+
+// Function Types as Return Types
+func stepForward(input: Int) -> Int {
+    return input + 1
+}
+
+func stepBackward(input: Int) -> Int {
+    return input - 1
+}
+
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+    return backwards ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(currentValue > 0)
+
+print("Counting to zero:")
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+// Nested Functions
+func chooseStepFunc(backwards: Bool) -> (Int) -> Int {
+    func stepFoward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backwards ? stepBackward : stepFoward
+}
+
+var currentCount = -5
+let moveToZero = chooseStepFunction(currentCount > 0)
+while currentCount != 0 {
+    print("\(currentCount)...")
+    currentCount = moveToZero(currentCount)
+}
+
+print("zero!")
