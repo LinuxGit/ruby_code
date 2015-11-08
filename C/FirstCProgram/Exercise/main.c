d2b2c4a5fcdb0944966aa1a7c5c50229adae860b
Louis <gofaceme@gmail.com>
2015-11-08 21:24:31 +0800

    Swift Inheritance

---
+++ C/FirstCProgram/Exercise/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  Exercise
+//
+//  Created by ruby on 15/11/8.
+//  Copyright © 2015年 LinuxGit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("*************\n");
+    printf("** hello c **\n");
+    printf("*************\n");
+    
+    int score;
+    score = 105;
+    
+    float f = 10.1f;
+    double  d = 10.0;
+    char c = 'a';
+    
+    return 0;
+}
