24fc40e97e0de1212bacdd3de11380eecd7c2ebf
Louis <gofaceme@gmail.com>
2015-11-10 00:17:17 +0800

    C: prinf scanf sizeof

--- C/FirstCProgram/Exercise/main.c
+++ C/FirstCProgram/Exercise/main.c
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 
-int main() {
+int test() {
     
     printf("*************\n");
     printf("** hello c **\n");
@@ -21,5 +21,55 @@ int main() {
     double  d = 10.0;
     char c = 'a';
     
+    printf("%d\n", score);
+    printf("%.2f\n", f);
+    printf("%06.2f\n", f);
+    printf("%c\n", c);
+    printf("%.2lf\n", d);
+    
+    printf("%d, %.2f, %.4lf, %c\n", score, f, d, c);
+    
+    int i = 1;
+    int amount = 9988;
+    printf("%03d\n", i);
+    printf("%3d\n", i);
+    printf("%-3d\n", i);
+    printf("%03d\n", amount);
+    
     return 0;
 }
+
+int test2() {
+    int i;
+    scanf("%i", &i);
+    printf("%d\n", i);
+    
+    float f;
+    scanf("%f", &f);
+    printf("%.2f\n", f);
+    
+    return 0;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d,%d,%d,%d",&a,&b,&c,&d);
+    printf("%d,%d,%d,%d\n",a,b,c,d);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
