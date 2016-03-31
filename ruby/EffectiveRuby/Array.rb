4b67ba991292378488e4a0ae1bf7d165ff0650a2
Louis <gofaceme@gmail.com>
2016-03-31 23:41:15 +0800

    Array method

---
+++ ruby/EffectiveRuby/Array.rb
@@ -0,0 +1,10 @@
+[1] pry(main)> Array('ruby')
+=> ["ruby"]
+[2] pry(main)> Array(nil)
+=> []
+[3] pry(main)> Array(['ruby', 'python'])
+=> ["ruby", "python"]
+[4] pry(main)> nil.to_a
+=> []
+[5] pry(main)> Array({'Louis': 20, 'Ruby': 18})
+=> [[:Louis, 20], [:Ruby, 18]]
