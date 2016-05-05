a4cbe7f9bbf9d230ab883a36c3e7d6422bb761ef
Louis <gofaceme@gmail.com>
2016-05-06 00:16:14 +0800

    Ruby alias_method

---
+++ ruby/EffectiveRuby/alias_method.rb
@@ -0,0 +1,22 @@
+module LogMethod
+  def log_method(method)
+    orig = "#{method}_without_logging".to_sym
+
+    if instance_methods.include?(orig)
+      raise(NameError, "#{orig} isn't a unique name")
+    end
+
+    alias_method(orig, method)
+
+    define_method(method) do |*args, &block|
+      $stdout.puts("calling method #{method}")
+      result = send(orig, *args, &block)
+      $stdout.puts("#{method} returned #{result.inspect}")
+      result
+    end
+  end
+end
+
+Array.extend(LogMethod)
+Array.log_method(:first)
+puts [1, 2, 3].first
