412a1b45d3142d6875f70d286c86be2abc5983a9
Louis <gofaceme@gmail.com>
2016-05-23 01:01:51 +0800

    add Lua table datatype

--- Lua/essentials/data_type.lua
+++ Lua/essentials/data_type.lua
@@ -9,3 +9,26 @@ local order = 3.7
 local score = 98.5
 print(math.floor(order))
 print(math.ceil(score))
+
+local str1 = [[add\nname]]
+local str2 = [=[string have a [[]].]=]
+print(str1)
+print(str2)
+
+local corp = {
+    web = "www.baidu.com",
+    lang = {'PHP', 'Python', 'Ruby'},
+    ["city"] = "Beijing",
+    10086
+}
+
+print(corp.web)
+print(corp.lang[3])
+print(corp["city"])
+print(corp[1])
+
+local function foo()
+    print("Hello Lua!")
+end
+
+print(type(foo))
